c8a770533633528a2e23061930d2e6d8ed915490
tim <tim@163.com>
2020-01-13 13:37:58 +0800

    测试一下

--- run_suite.py
+++ run_suite.py
@@ -12,18 +12,18 @@ suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestEmployee))
 
 # 执行测试套件
-# unittest.TextTestRunner().run(suite)
+unittest.TextTestRunner().run(suite)
 
 # 定义测试报告文件路径
 # report_path = app.BASE_DIR + "/report/report-{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
 report_path = app.BASE_DIR + "/report/report.html"
 
 # 打开文件流
-with open(report_path, "wb") as f:
-    # 创建HTMLTestRunner运行器对象
-    runner = HTMLTestRunner(f, title="IHRM接口测试报告", description="V1.0")
-
-    # 执行测试套件
-    runner.run(suite)
-
-    print("over....")
+# with open(report_path, "wb") as f:
+#     # 创建HTMLTestRunner运行器对象
+#     runner = HTMLTestRunner(f, title="IHRM接口测试报告", description="V1.0")
+#
+#     # 执行测试套件
+#     runner.run(suite)
+#
+#     print("over....")
